2044652c4375141ae08ed0d86e90e40de145d480
Facundo <fbayona13@gmail.com>
2022-06-09 20:06:43 -0300

    prueba de heroku

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const path = require("path");
 const express = require("express");
 const server = express();
+const port = process.env.PORT || 2020
 
-server.listen(3000, () => console.log("PRUEBA DE SERVIDOR MERCADO LIEBRE"));
+server.listen(3000, () => console.log("PRUEBA DE SERVIDOR MERCADO LIEBRE puerto_2020"));
 
 const public = path.resolve(__dirname, "../public");
 server.use(express.static(public));
