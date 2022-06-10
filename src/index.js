c164c9e1cd3a4ae59500e7a8a96884cf4a6374c2
Facundo <fbayona13@gmail.com>
2022-06-10 12:54:35 -0300

    agrega paginas de register y login

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ const express = require("express");
 const server = express();
 const port = process.env.PORT || 2020
 
-server.listen(port, () => console.log("PRUEBA DE SERVIDOR MERCADO LIEBRE puerto_2020"));
+server.listen(port, () => console.log("mercadoLiebre starting at port http://localhost:" + port));
 
-const public = path.resolve(__dirname, "../public");
+let public = path.resolve(__dirname, "../public");
 server.use(express.static(public));
+
+let views = path.resolve(__dirname, "../src/views");
+server.use(express.static(views));
 
 server.get("/", (req, res) =>
   res.sendFile(path.resolve(__dirname, "views/home.html"))
+);
+
+server.get("/login", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views/login.html"))
+);
+
+server.get("/register", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views/register.html"))
 );
